33c8273c986e5c3aa80a37634e4e9d13c81553c2
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-12-10 12:17:14 +0200

    add quadtree.js

--- src/quadtree/quadtree.js
+++ src/quadtree/quadtree.js
@@ -1,21 +1,56 @@
+import Rectangle from "./rectangle";
+
 export default class QuadTree {
     constructor(boundary, capacity){
         this.boundary = boundary;
         this.capacity = capacity;
         this.points = [];
-        this.divisions = [];
+        this.divs = [];
         this.divided = false;
     }
 
     insert(point){
-        if (this.points.length > this.capacity){
-            subdivide()
+        if (this.boundary.contains(point)){return false;}
+
+        if (this.points.length < this.capacity && !this.divided){
+            this.points.push(point);
+            return true;
+        } else {
+            if (!this.divided){
+                subdivide();
+            }
+            
+            for (let div of this.divs){
+                if(div.insert(point)){return true;}
+            }
         }
     }
 
     subdivide(){
         let x = this.boundary.xPos;
         let y = this.boundary.yPos;
-        let width = this.boundary.width;
+        let w = this.boundary.width;
+        let h = this.boundary.height;
+
+        let nw = new Rectangle(x,y,w/2,h/2);
+        let ne = new Rectangle(x+w/2,y,w/2,h/2);
+        let sw = new Rectangle(x,y+h/2,w/2,h/2);
+        let se = new Rectangle(x+w/2,y+h/2,w/2,h/2);
+        const cap = this.capacity+1;
+
+        this.divs = [
+            new QuadTree(nw, cap),
+            new QuadTree(ne, cap),
+            new QuadTree(sw, cap),
+            new QuadTree(se, cap)];
+        this.divided = true;
+
+        for (let point of this.points){
+
+        }
+    }
+
+    draw(ctx){
+        
     }
 } 
