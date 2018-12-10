631b3d2b1daa60101d1dea46e1eaadbff38ddb49
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-12-10 12:17:41 +0200

    add engine.js

--- src/engine.js
+++ src/engine.js
@@ -1,3 +1,6 @@
+import QuadTree from "./quadtree/quadtree";
+import Rectangle from "./quadtree/rectangle";
+
 export default class Engine {
     constructor(){
         document.body.style.margin = "0px";
@@ -10,8 +13,24 @@ export default class Engine {
         document.body.appendChild(this.canvas);
         this.ctx = this.canvas.getContext("2d");
 
+        // setup QuadTree
+        this.boundary = new Rectangle(0,0,this.canvas.width,this.canvas.height);
+        this.qTree = new QuadTree(this.boundary, 1);
+
+        // Important variables
+        this.objects = []; 
+
+        // debug setup
+        this.debug = true;
+
         // Start the loop
         window.requestAnimationFrame(this.loop.bind(this));
+    }
+
+    draw(){
+        if (debug){
+            this.qTree.draw(this.ctx)
+        }
     }
 
     loop(){
