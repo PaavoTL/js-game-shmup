9370135da7723ea1842021646e9c252ddc667bea
PaavoTL <43340576+PaavoTL@users.noreply.github.com>
2018-12-10 12:17:00 +0200

    add rectangle.js

--- src/quadtree/rectangle.js
+++ src/quadtree/rectangle.js
@@ -5,4 +5,18 @@ export default class Rectangle {
         this.width = width;
         this.height = height;
     }
+
+    contains(point){
+        return (point.x >= this.xPos &&
+            point.x <= this.xPos + this.width &&
+            point.y >= this.yPos &&
+            point.y <= this.yPos + this.height);
+    }
+
+    intersects(range){
+        return !(range.x > this.xPos + this.width ||
+            range.x + range.w < this.xPos ||
+            range.y > this.yPos + this.height ||
+            range.y + range.h < this.yPos);
+    }
 }
